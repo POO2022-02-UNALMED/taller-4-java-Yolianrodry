de78ea2ff786133840237e3015db7d4ab9a7a493
Yolian <105956749+Yolianrodry@users.noreply.github.com>
2022-09-05 17:42:31 -0500

    v

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -11,7 +11,6 @@ public class Asignatura {
     Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
     }
-
     Asignatura(int codigoExterno,Tipo tipo) {
         this("Sin nombre", 0, codigoExterno,tipo);
     }
@@ -19,33 +18,27 @@ public class Asignatura {
     Asignatura(String nombre, Tipo tipo) {
         this(nombre, 0, 0,tipo);
     }
-
     Asignatura(String nombre, int codigoInterno, int codigoExterno,Tipo tipo) {
         this.nombre = nombre;
         this.codigoInterno = codigoInterno;
         this.codigoExterno = codigoExterno;
         this.tipo = tipo;
     }
-
     void cambiarDatos(int codigoInterno, int codigoExterno, String nombre,Tipo tipo) {
         this.codigoInterno = codigoInterno;
         this.codigoExterno = codigoExterno;
         this.nombre = nombre;
         this.tipo = tipo;
     }
-
     void cambiarDatos(int codigoExterno) {
         this.codigoExterno = codigoExterno;
     }
-
     void cambiarDatos(String nombre) {
         this.nombre = nombre;
     }
-
     public void setCodigoInterno(int codigoInterno) {
         this.codigoInterno = codigoInterno;
     }
-
     public void setCodigoInterno(double codigoInterno) {
         this.codigoInterno = (int) codigoInterno;
     }
